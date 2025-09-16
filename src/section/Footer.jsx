c64b1fcbf0f881a7d9b0761191877770d9dea177
jostust <josephtussiet@gmail.com>
2025-09-15 22:01:57 -0600

    Delete src/section directory

--- src/section/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="py-6 border-t border-line text-center">
-      <div className="container-outer">
-        <p className="text-[#8a8f98]">© 2025 א‑Reset · Términos · Privacidad</p>
-      </div>
-    </footer>
-  )
-}
